517cbe1b69e209bdbe957921b1b92b4d0e165be4
bhandhariya <sainiraja59@gmail.com>
2019-07-27 12:10:41 +0530

    mmaterial form created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,10 @@ import { SurveyComponent } from './survey/survey.component';
 import { Survey2Component } from './survey2/survey2.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatRadioModule} from '@angular/material/radio';
-
+import {MatInputModule} from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import {MatSelectModule} from '@angular/material/select';
 
 @NgModule({
   declarations: [
@@ -24,7 +27,7 @@ import {MatRadioModule} from '@angular/material/radio';
     AppRoutingModule,
     HttpClientModule,
     FormsModule,ReactiveFormsModule, BrowserAnimationsModule,
-    MatRadioModule
+    MatRadioModule,MatInputModule,MatDatepickerModule,MatNativeDateModule,MatRadioModule,MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
